0cd9216a55a75d8a6b464e12ae1d091377a1c8cd
rasuljon <anvarovr87@gmail.com>
2024-02-10 10:08:20 +0300

    index.html

---
+++ main.js
@@ -0,0 +1,25 @@
+
+let a = prompt( "ismingiz nima")
+console.log( "sizning ismingiz: " + a );
+
+let b = prompt( a + " Yoshingiz nechida?")
+console.log( a + " Yoshingiz " + b );
+
+let c = "6 + 6"
+let d = prompt( a + " misollarni yechishga harakat qiling: " + c )
+console.log( "Misol-1: " + "("+c+")" + " = " + d );
+
+let e ="7 * 7"
+let f = prompt(e) 
+console.log("Misol-2: " + "("+e+")" + " = " + f );
+
+let j = "7 + 7"
+let h = prompt(j)
+console.log( "Misol-3: " + "("+j+")" + " = " + h );
+
+let i = "10 / 7"
+let g = prompt (i)
+console.log("Misol-4: " + "("+i+")" + " = " + g );
+
+let k = "javobingizni CONSOLEdan korishingiz mumkun"
+alert(k)
